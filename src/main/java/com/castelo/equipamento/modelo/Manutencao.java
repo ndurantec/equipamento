17aa16351485c5463d2d4f485929c17754c2eeb6
nanojha12 <jose.altissimo@escola.pr.gov.br>
2024-08-15 10:14:47 -0300

    criando as classes

---
+++ src/main/java/com/castelo/equipamento/modelo/Manutencao.java
@@ -0,0 +1,151 @@
+package com.castelo.equipamento.modelo;
+
+import java.sql.Date;
+
+public class Manutencao {
+    
+    private String local;
+    private double telefone;
+    private String email;
+    private Date data;
+    private float custo;
+    private Equipamento equipamento;
+    private String nome;
+
+    public Manutencao() {
+    }
+
+    public Manutencao(String local, double telefone, String email, Date data, float custo, Equipamento equipamento,
+            String nome) {
+        this.local = local;
+        this.telefone = telefone;
+        this.email = email;
+        this.data = data;
+        this.custo = custo;
+        this.equipamento = equipamento;
+        this.nome = nome;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public double getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(double telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public float getCusto() {
+        return custo;
+    }
+
+    public void setCusto(float custo) {
+        this.custo = custo;
+    }
+
+    public Equipamento getEquipamento() {
+        return equipamento;
+    }
+
+    public void setEquipamento(Equipamento equipamento) {
+        this.equipamento = equipamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((local == null) ? 0 : local.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(telefone);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((data == null) ? 0 : data.hashCode());
+        result = prime * result + Float.floatToIntBits(custo);
+        result = prime * result + ((equipamento == null) ? 0 : equipamento.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Manutencao other = (Manutencao) obj;
+        if (local == null) {
+            if (other.local != null)
+                return false;
+        } else if (!local.equals(other.local))
+            return false;
+        if (Double.doubleToLongBits(telefone) != Double.doubleToLongBits(other.telefone))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (data == null) {
+            if (other.data != null)
+                return false;
+        } else if (!data.equals(other.data))
+            return false;
+        if (Float.floatToIntBits(custo) != Float.floatToIntBits(other.custo))
+            return false;
+        if (equipamento == null) {
+            if (other.equipamento != null)
+                return false;
+        } else if (!equipamento.equals(other.equipamento))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Manutencao [local=" + local + ", telefone=" + telefone + ", email=" + email + ", data=" + data
+                + ", custo=" + custo + ", equipamento=" + equipamento + ", nome=" + nome + "]";
+    }
+
+    
+    
+
+}
