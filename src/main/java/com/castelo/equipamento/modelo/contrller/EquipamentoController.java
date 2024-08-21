37be16a451e3aaf2410c100a48a5cb8ff7a59765
150Victor150 <anthoni.cardoso@escola.pr.gov.br>
2024-08-21 12:16:32 -0300

    criei o controller

---
+++ src/main/java/com/castelo/equipamento/modelo/contrller/EquipamentoController.java
@@ -0,0 +1,15 @@
+package com.castelo.equipamento.modelo.contrller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/equipamento")
+public class EquipamentoController {
+
+    @GetMapping(value = "/imprimir")
+    public void imprimir(){
+        System.out.println("chegou no servidor");
+    }
+}
