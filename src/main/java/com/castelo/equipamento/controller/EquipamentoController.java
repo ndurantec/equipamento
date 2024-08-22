11e010c498e6a35e9ebd3a35bbcbb7486d21271b
AnthOnnii <anthoni.cardoso@escola.pr.gov.br>
2024-08-22 11:13:33 -0300

    movi o controller

--- src/main/java/com/castelo/equipamento/controller/EquipamentoController.java
+++ src/main/java/com/castelo/equipamento/controller/EquipamentoController.java
@@ -1,4 +1,4 @@
-package com.castelo.equipamento.modelo.contrller;
+package com.castelo.equipamento.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
