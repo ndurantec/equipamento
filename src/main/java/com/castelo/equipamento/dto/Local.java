22889c43de37f2a8860a10d44be26c59e28bf561
marquim511 <marco.franco@escola.pr.gov.br>
2024-08-22 11:34:07 -0300

    criei todos os DTOs

---
+++ src/main/java/com/castelo/equipamento/dto/Local.java
@@ -0,0 +1,19 @@
+package com.castelo.equipamento.dto;
+
+public class Local {
+
+    private String nome;
+
+    public Local() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
