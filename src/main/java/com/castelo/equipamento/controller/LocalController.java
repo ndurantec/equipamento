4322de24f340f00f258a449909d42a195ac9be4c
wellington-wm <wellington.nascimento.silva@escola.pr.gov.br>
2024-08-22 12:10:03 -0300

    criei local controller

---
+++ src/main/java/com/castelo/equipamento/controller/LocalController.java
@@ -0,0 +1,33 @@
+package com.castelo.equipamento.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/local")
+public class LocalController {
+
+    @GetMapping(value = "/imprimir")
+    public String imprimir(){
+        return "chegou no servidor";
+    }
+
+    @PostMapping(value = "/cadastrar")
+    public void cadastrar(){
+        System.out.println("cadastrou com sucesso");
+    }
+
+    @PutMapping(value = "/atualizar")
+    public void atualizar(){
+        System.out.println("atulizando concluida");
+    }
+    
+    @DeleteMapping(value = "/deletar")
+    public void deletar(){
+        System.out.println("deletado com sucesso");
+    }
+}
