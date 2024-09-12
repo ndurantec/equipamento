251b336d6dbb927302b49744fa774b129cab78ff
josealtissimo <jose.altissimo@escola.pr.gov.br>
2024-09-12 10:49:40 -0300

    fiz equipamento

--- src/main/java/com/castelo/equipamento/controller/EquipamentoController.java
+++ src/main/java/com/castelo/equipamento/controller/EquipamentoController.java
@@ -1,7 +1,6 @@
 package com.castelo.equipamento.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
